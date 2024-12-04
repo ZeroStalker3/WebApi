91fc9ce87c0a6450a0b9557ace4f822d044b8e1d
ZeroStalker3 <stalker_mr@list.ru>
2024-12-04 20:28:33 +0900

    update

--- WeatherForecast.cs
+++ WeatherForecast.cs
@@ -3,7 +3,7 @@ namespace WebApi
     public class WeatherForecast
     {
         public DateOnly Date { get; set; }
-
+         
         public int TemperatureC { get; set; }
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
